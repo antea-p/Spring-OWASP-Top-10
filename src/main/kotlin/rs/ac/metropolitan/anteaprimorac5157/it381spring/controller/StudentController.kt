abc8bd4c20ad4d302fc7f9215fe77857f17c50c8
Antea P <antea.primorac0@gmail.com>
2024-11-20 17:06:07 +0100

    added grade and comment, updated Controller and details page accordingly

--- src/main/kotlin/rs/ac/metropolitan/anteaprimorac5157/it381spring/controller/StudentController.kt
+++ src/main/kotlin/rs/ac/metropolitan/anteaprimorac5157/it381spring/controller/StudentController.kt
@@ -1,6 +1,7 @@
 package rs.ac.metropolitan.anteaprimorac5157.it381spring.controller
 
 import org.springframework.http.HttpStatus
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,23 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.server.ResponseStatusException
 import rs.ac.metropolitan.anteaprimorac5157.it381spring.model.Student
+import java.util.*
 
 @Controller
 class StudentController {
     val studentsList = listOf(
-        Student(1, "Amanda Smith"),
-        Student(2, "Charlie Brown"),
-        Student(3, "Bob Johnson"),
+        Student(1, "Amanda Smith", 7.95, "Test comment 1"),
+        Student(2, "Charlie Brown", 6.50, "Test comment 2"),
+        Student(3, "Bob Johnson", 9.75, "Test comment 3"),
     )
 
-    @GetMapping("/students")
-    fun students() = "students"
-
     @GetMapping("")
-    fun listStudents(model: Model): String {
+    fun home() = "home"
+
+    @GetMapping("/students")
+    fun students(model: Model): String {
         model.addAttribute("students", studentsList)
         return "students"
     }
+
 
     @GetMapping("/students/{id}")
     fun getStudent(@PathVariable id: Long, model: Model): String {
--- src/main/kotlin/rs/ac/metropolitan/anteaprimorac5157/it381spring/model/Student.kt
+++ src/main/kotlin/rs/ac/metropolitan/anteaprimorac5157/it381spring/model/Student.kt
@@ -3,4 +3,6 @@ package rs.ac.metropolitan.anteaprimorac5157.it381spring.model
 data class Student(
     val id: Long? = null,
     val name: String,
+    val grade: Double,
+    val comment: String,
 )
